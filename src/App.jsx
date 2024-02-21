8aa14c427d565a1d951a524f043757ef86a6878c
Derzsi Dezso~ <derzsidezso@gmail.com>
2024-02-21 17:12:18 +0200

    added components and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+import data from "./data.js";
+import List from "./List.jsx";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <section className="container">
+      <h3>{people.length}birthdays today</h3>
+      <List people={people} />
+      <button
+        type="button"
+        className="btn btn-block"
+        onClick={() => setPeople([])}
+      >
+        clear all
+      </button>
+    </section>
+  );
 };
 export default App;
---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import Person from "./Person.jsx";
+
+const List = ({ people }) => {
+  return (
+    <section>
+      {people.map((person) => {
+        return <Person key={person.id} {...person} />;
+      })}
+    </section>
+  );
+};
+export default List;
